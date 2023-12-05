6870c3bf216e8dae5b7dcfb17bb23f45211e9b54
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-06 02:53:17 +0800

    Basic Calculator

--- Calculator/Calculator/Utility.hpp
+++ Calculator/Calculator/Utility.hpp
@@ -7,14 +7,14 @@ namespace utility
 {
     // Print result
     template <typename T>
-    [[nodiscard]] void print(const T& result) noexcept
+    void print(const T& result) noexcept
     {
         std::cout << result << std::endl;
     }
 
 
     // Continue?
-    [[nodiscard]] void proceed(char& flag) noexcept
+    void proceed(char& flag) noexcept
     {
         std::cout << "Do you want to continue (Enter y or n): ";
         std::cin >> flag;
@@ -22,7 +22,7 @@ namespace utility
 
 
     // End
-    [[nodiscard]] void thanks() noexcept
+    void thanks() noexcept
     {
         std::cout << "Thank you so much for using the program." << std::endl;
     }
